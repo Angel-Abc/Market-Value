71ae1f5f866c27d861042575ee916626eed0af16
AngelAbc <AngelAbc@AngelAbc.com>
2025-04-04 21:27:25 +0200

    Created empty typescript application with TailwindCss

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { stat } = require('fs')
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: [/node_modules/],
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    ['@tailwindcss/postcss', {}]
+                                ]
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        port: 3000
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+}
